7e3b3062cf1cecbf9d97c052156ecadb42a52279
Goderich <yuhsien77@gmail.com>
2024-05-06 16:28:36 +0800

    Automate running tests (Make, build, run)

--- impls/zig/build.zig
+++ impls/zig/build.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const exe = b.addExecutable(.{
-        .name = "step1",
+        .name = "step1_read_print",
         .root_source_file = .{ .path = "step1_read_print.zig" },
         .target = b.host,
     });
